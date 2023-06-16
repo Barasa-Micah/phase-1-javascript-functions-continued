0479386d9aac063bbad49b6123c6f878244e72af
Barasa-Micah <barasamica@gmail.com>
2023-06-16 12:41:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun('bathe my dog');
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork('work from home.');
+
+function wrapAdjective(cute = '*'){
+    return function(cat= 'special'){
+        return `You are a ${cute}${cat}${cat}!`
+    }
+}
+
+wrapAdjective ('%')('a dedicated programmer');
